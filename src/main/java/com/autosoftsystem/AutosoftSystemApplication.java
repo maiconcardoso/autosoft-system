7ba02c460ededc8117085df57e0dfd2157dc4ebc
maicon_cardoso <maiconscardoso@hotmail.com>
2022-05-05 21:19:04 -0300

    initial commit

---
+++ src/main/java/com/autosoftsystem/AutosoftSystemApplication.java
@@ -0,0 +1,13 @@
+package com.autosoftsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosoftSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosoftSystemApplication.class, args);
+	}
+
+}
